30552f8671cec64f610ea27714249a05eb83b8de
gperretta <113616815+gperretta@users.noreply.github.com>
2022-11-16 19:32:42 +0100

    #1 - creating a project.

---
+++ UnicoCampaniaRedesign/UnicoCampaniaRedesign/UnicoCampaniaRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnicoCampaniaRedesignApp.swift
+//  UnicoCampaniaRedesign
+//
+//  Created by Gilda on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct UnicoCampaniaRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
